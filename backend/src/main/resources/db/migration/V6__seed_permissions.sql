7b5db3340da51b7492ede2a176f47d5d763557ef
thanhknghien <thanh2005dinh@gmail.com>
2026-08-10 10:23:33 +0700

    fix error sql format on file V6 migration

--- backend/src/main/resources/db/migration/V6__seed_permissions.sql
+++ backend/src/main/resources/db/migration/V6__seed_permissions.sql
@@ -1,7 +1,7 @@
 INSERT INTO permissions (name, description)
 VALUES ('task:read_any', 'Read any task'),
-       ('task:write_any', 'Write any task')
-       ('user:read_any', 'Read users')
-       ('user:manage', 'Manage users')
-       ('role:manage', 'Manage roles')
+       ('task:write_any', 'Write any task'),
+       ('user:read_any', 'Read users'),
+       ('user:manage', 'Manage users'),
+       ('role:manage', 'Manage roles'),
        ('audit:read', 'Read audit logs');
